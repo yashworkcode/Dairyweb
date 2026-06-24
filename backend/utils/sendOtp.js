ee8084a6b0b2ace1c082b4b4d70183eb41c5f46a
yashworkcode <yashworkcode@gmail.com>
2026-06-25 00:43:07 +0530

    fix 19

--- backend/utils/sendOtp.js
+++ backend/utils/sendOtp.js
@@ -8,7 +8,9 @@ const generateOtpCode = () => {
 };
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
